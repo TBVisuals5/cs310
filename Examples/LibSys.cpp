d31b001f4750e33de2c46123c8cd5e1e2d9c4d17
TBVisuals5 <163580144+TBVisuals5@users.noreply.github.com>
2025-08-28 19:41:43 +0000

    LibSystem

---
+++ Examples/LibSys.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+namespace LibSystem{
+
+    using std::string
+
+    struct BookRecord{
+
+    sring title;
+    string author;
+    int id;
+
+    void setDetails(const string& t, const string& a, int i){
+
+        title = t;
+        author = a;
+        int = i;
+
+    }
+};
