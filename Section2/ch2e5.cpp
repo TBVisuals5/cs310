18e15c619ba6683ba13fd76f4da5f8b34a96f400
TBVisuals5 <163580144+TBVisuals5@users.noreply.github.com>
2025-09-04 20:04:00 +0000

    finish

--- Section2/ch2e5.cpp
+++ Section2/ch2e5.cpp
@@ -7,8 +7,25 @@ class CS310Exception : public exception{
 
     private:
         string message;
+
     public:
-        CS310Exception(const string& msg): message(msg)(){
-            const char* what() c
+        CS310Exception(const string& msg): message(msg){}
+
+        const char* what() const noexcept override{
+            return message.c_str();
         }
+        
+};
+
+int main(){
+    
+    try 
+    {
+        throw CS310Exception(" Hi this is fun!");
+    }
+        catch(const CS310Exception& e)
+        {
+            cerr << e.what() << '\n';
+        }
+        return 0;
 }
