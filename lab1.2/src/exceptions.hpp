aca49d37139f4028b55f07eb7502be28114e7c8a
TBVisuals5 <163580144+TBVisuals5@users.noreply.github.com>
2025-09-05 14:51:02 +0000

    Lab1.2

---
+++ lab1.2/src/exceptions.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include <stdexcept>
+using namespace std;
+
+struct ValidationError   : runtime_error { using runtime_error::runtime_error; };
+struct SerializationError: runtime_error { using runtime_error::runtime_error; };
