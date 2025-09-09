d3992446566bf8ee88611dfd940c56dea5786f7f
TBVisuals5 <163580144+TBVisuals5@users.noreply.github.com>
2025-09-09 19:41:20 +0000

    Array Base

---
+++ Section2/Chapter-3/che3e1.cpp
@@ -0,0 +1,87 @@
+#include <iostrem>
+
+class ItemType
+{
+    public:
+        int key;
+        ItemType(int k): key(k){}
+
+        int CompareTo(ItemType other)
+        {
+
+            if(key == other.key)
+            {
+                return 0,
+            }
+            else if(key < other.key)
+            {
+                return -1
+            }
+            else
+            {
+                return 1;
+            }
+        }
+};
+
+const int MAX_ITEMS = 100;
+class UnsortedLists
+{
+
+    private:
+        ItemType data[MAX_ITEMS];
+        int length = 0;
+        int currPos = -1;
+
+    public:
+        UnsortedLists() = default;
+
+        ~UnsortedLists()
+        {
+            delete[] data;
+        }
+
+        void Insert(ItemType item)
+        {
+            if (length < MAX_ITEMS )
+            {
+                data[length++] = item;
+            }
+        }
+
+        int length() const
+        {
+            return length
+        }
+
+        bool isEmpty()
+        {
+            return length == 0;
+        }
+
+        bool isFull()
+        {
+            return length == MAX_ITEMS;
+        }
+
+        void ResetList()
+        {
+            currPos = -1;
+        }
+
+        void delete(ItemType item)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (data[i].CompareTo(item)==0)
+                {
+                    for (int j=i; j < lenght-1; j++)
+                    {
+                        data[j] = data[j+1];
+                    }
+                    lenght--;
+                    return;
+                }
+            }
+        }
+};
